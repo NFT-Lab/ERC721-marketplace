19f2bae717a88b1d8a6f78d7952058b2c2c02709
badadumTss <lucazanny@gmail.com>
2021-07-14 19:14:28 +0200

    get active trade of nft

--- test/ETHMarketplace/getTrades.test.ts
+++ test/ETHMarketplace/getTrades.test.ts
@@ -71,4 +71,21 @@ describe("ETHMarketplace - get trades", function () {
       .to.contain(signers[1].address)
       .and.to.contain(ethers.utils.formatBytes32String("Open"));
   });
+
+  it("Should get all the active trades", async () => {
+    nftLabStore.mint(signers[1].address, NFT);
+    const tokenID = await nftLabStore.tokenOfOwnerByIndex(
+      signers[1].address,
+      0
+    );
+    expect(await nftLabMarketplace.connect(signers[1]).openTrade(tokenID, 1))
+      .to.emit(nftLabMarketplace, "TradeStatusChange")
+      .withArgs(0, "Open");
+
+    const trade = await nftLabMarketplace
+      .connect(signers[1])
+      .getTradeOfNft(tokenID)
+
+    expect(await nftLabMarketplace.getTrade(trade)).not.to.be.null;
+  });
 });
